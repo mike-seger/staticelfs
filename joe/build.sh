4480ffb8cfbcfe345973a73fa0fc4a3a32d81e07
michael <michael@n.c>
2018-11-02 23:41:05 +0000

    included Dockerfile and build script

---
+++ joe/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+docker rmi --force centoslatest
+rm -fR bin.tmp
+name=N$(date +%s)
+docker build -t centoslatest .
+docker run --name $name -d centoslatest /bin/bash
+mkdir bin.tmp
+docker cp $name:/opt/bin/. bin.tmp
+docker rmi --force centoslatest
+mv bin bin.old
+mv bin.tmp bin
+rm -fR bin.old
